d25202df2f211e60c2b7df6db73efa853caa36b9
cestayoyarzo1 <carlos.estay@gmail.com>
2018-11-28 14:12:59 -0700

    scrollview script added, not fully working yet

---
+++ FINAL PROJECT/Assets/MyScripts/ScrollViewScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewScript : MonoBehaviour
+{
+    public List<GameObject> messagelist = new List<GameObject>();
+    int maxMessages = 30;
+
+    public GameObject panel;
+    public GameObject textObject;
+    public Scrollbar verticalScroll;
+    //public Toggle autoScroll;
+
+    float timer = 0.0f;
+    int counter = 0;
+
+    bool autoscroll;
+
+    void Start()
+    {
+        //autoScroll.isOn = false;
+        autoscroll = true;
+    }
+
+    void Update()
+    {
+        if (timer > 1)
+        {
+            counter += (int)timer;
+            GameObject newTextObject = Instantiate(textObject, panel.transform);
+            newTextObject.GetComponent<Text>().text = counter.ToString() + " seconds have gone by";
+            AddMessage(newTextObject);
+            timer = 0.0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+    }
+    public void AddMessage(GameObject messageObject)
+    {
+        if (autoscroll)
+        {
+            verticalScroll.value = 0.0f;
+        }
+        else if (!autoscroll)
+        {
+            verticalScroll.value = Mathf.Min(1.0f, verticalScroll.value + 1.0f / maxMessages);
+        }
+
+        messagelist.Add(messageObject);
+        if (messagelist.Count >= maxMessages)
+        {
+            Destroy(messagelist[0].gameObject);
+            messagelist.Remove(messagelist[0]);
+        }
+    }
+
+    public void AutoScroll()
+    {
+        //verticalScroll.value = 0;
+        autoscroll = true;
+    }
+}
